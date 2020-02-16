9c9e03f9a85d8c875952f0c6a652239a33d04be3
Omar Khan <o.k@berkeley.edu>
2020-02-16 03:05:58 -0800

    now display entire history of commits

--- parser.py
+++ parser.py
@@ -1,9 +1,9 @@
-""" Creates two CSVs that represent the graph of the N most recent commits. 
+""" Creates two CSVs that represent the history of commits via graph. 
 
 Use as follows:
 
 
-    python parser.py <insert path to git repository here> <number of commits here>
+    python parser.py <insert path to git repository here>
 
     If you want to create the csv over the current git repository, insert "." for the path
 
@@ -16,17 +16,54 @@ import operator
 import pandas as pd
 
 
-""" Returns a List of String filenames from the given Commit object. """
-def get_blob_names_of_commit(commit_obj):
-    tree = commit_obj.tree
-    blob_names = []
-    for blob in tree.blobs():
-        blob_names.append(blob.name)
-    return blob_name
 
 
-""" Returns (1) a Dictionary of the N most recent Commit objects keyed by name of branch and
-            (2) a List of Edges of the N most recent commits of REPO on the current branch. 
+def update_commits(commits, history1, history2, branch1, branch2, head1, head2, visited):
+    visited1 = set([c.hexsha for c in history1])
+
+    for c in history1:
+        if c.hexsha not in visited:
+            commits[branch1.name].append(c)
+            visited.add(c.hexsha)
+
+    for c in history2:
+        if c.hexsha not in visited:
+            visited.add(c.hexsha)
+            if c not in visited1:
+                commits[branch2.name].append(c)
+
+    
+
+
+""" COMMIT is the result of a merge. Updates dictionaries accordingly """
+def mark_levels_from_merge(commit, visited_commit_shas, commit_to_level, edges):
+    level = commit_to_level[commit]
+    commit_to_level[commit.parents[0]] = level
+    commit_to_level[commit.parents[1]] = level + 1
+
+    for parent in commit.parents:
+        visited_commit_shas.add(parent.hexsha)
+        edges.append( (commit, parent) )
+
+    stack = [commit.parents[0], commit.parents[1]]
+    while stack != []:
+        commit = stack.pop()
+
+        if len(commit.parents) == 2:
+            mark_levels_from_merge(commit, visited_commit_shas, commit_to_level)
+        elif len(commit.parents) == 1:
+            parent = commit.parents[0]
+            if parent.hexsha in visited_commit_shas:
+                continue
+            else:
+                commit_to_level[parent] = commit_to_level[commit]
+                edges.append( (commit, parent) )
+
+            
+
+
+""" Returns (1) a Dictionary all the Commit objects keyed by name of branch and
+            (2) a List of Edges of all the Commits of REPO on the current branch. 
 
     Edges are in the form: (child, parent) to denote that PARENT has a directed edge to CHILD
 
@@ -38,17 +75,19 @@ def get_blob_names_of_commit(commit_obj):
                 ^
                 |
 
-    Where the HEAD pointer is at commit F. Note that commits D and C don't matter in this case. Say N = 4.
+    Where the HEAD pointer is at commit F. 
 
     The Dictionary of Commit objects we will return is:
 
-    { "MASTER" : [F, E, B, A] }
+    { "MASTER" : [F, E, B, A], "OTHER" : [D, C] }
 
     and the list of Edges will be:
     [
         [F, E],
         [E, B],
-        [B, A]
+        [B, A],
+        [D, C],
+        [C, B]
     ]
 
 
@@ -60,10 +99,10 @@ def get_blob_names_of_commit(commit_obj):
                 ^
                 |
 
-    Where the HEAD pointer is at commit F. If we want the 5 most recent commits, we'll return this:
+    Where the HEAD pointer is at commit F. 
 
-    If N = 5, the Dictionary of Commit objects we'll return is:
-    { "MASTER" : [F, E, B], "OTHER" : [D, C]" }
+    Dictionary of Commit objects we'll return is:
+    { "MASTER" : [F, E, B, A], "OTHER" : [D, C]" }
 
     and the list of Edges will be:
 
@@ -72,37 +111,64 @@ def get_blob_names_of_commit(commit_obj):
         [F, E],
         [D, C],
         [E, B],
-        [C, B]
+        [C, B],
+        [B, A]
     ]
 
 """
-def n_most_recent_commits(repo, n):
+def get_commits_and_edges(repo):
 
-    """ Gets the most recent commit and its branch name. """
-    queue = [ (repo.active_branch.name, repo.commit()) ]
-    """ Already added the most recent commit. """
-    i = 1
     commits = {} 
+    edges = []
+    visited_commit_shas = set()
+
+    """ Gets the most recent commit on each branch. """
+    for branch in repo.branches:
+        head = branch.commit
+        commits[branch.name] = [head]
+        visited_commit_shas.add(head.hexsha)
+
+        c = head
+        while len(c.parents) > 0:
+            for parent in c.parents:
+                edges.append( (c, parent) )
+            c = c.parents[0]
+            if c.hexsha not in visited_commit_shas:
+                visited_commit_shas.add(c.hexsha)
+                commits[branch.name].append(c)
+
+    n = len(repo.branches)
+    for i in range(n):
+        for j in range(i + 1, n):
+            branch1 = repo.branches[i]
+            branch2 = repo.branches[j]
+            head1 = branch1.commit
+            head2 = branch2.commit
+            history1 = [c for c in head1.traverse()]
+            history2 = [c for c in head2.traverse()]
+
+            update_commits(commits, history1, history2, branch1, branch2, head1, head2, visited_commit_shas)
+    return commits, edges
+"""
     visited_commit_shas = set()
     edges = []
-    while queue != []:
-        print(queue)
-        branch_name, commit = queue.pop(0)
+    while stack != []:
+        level, commit = queue.pop(0)
         if commit.hexsha not in visited_commit_shas:
             visited_commit_shas.add(commit.hexsha)
         else:
-            print(commit.hexsha, branch_name)
             continue
-        print(commit.hexsha, branch_name)
         if branch_name not in commits:
             commits[branch_name] = [commit]
         else:
             commits[branch_name].append(commit)
 
-        is_merge = len(commit.parents) > 1
+        is_merge = len(commit.parents) = 2
+        if is_merge:
+            mark_levels_from_merge(commit, visited_commit_shas, commit_to_level, commits, edges)
+            continue
         to_add = []
         for parent_commit in commit.parents:
-            """ Done adding commits. """
             if i == n:
                 break
             i += 1
@@ -119,7 +185,7 @@ def n_most_recent_commits(repo, n):
             to_add = [ (branch_name, parent_commit) ] + to_add
             edges.append( (commit, parent_commit) )
         queue.extend(to_add)
-    return commits, edges
+        """
 
 
 """ Creates a dictionary from the commit to fit the columns of commits.csv as described in the 
@@ -179,12 +245,13 @@ def export_to_csv(commits_by_branch, edges):
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 3:
-        print("please provide the path of the git repository and the number of commits to include")
+    if len(sys.argv) != 2:
+        print("please provide the path of the git repository")
     else:
         repo = Repo(sys.argv[1])
-        n = sys.argv[2]
 
-        commits, edges = n_most_recent_commits(repo, n)
+        commits, edges = get_commits_and_edges(repo)
         export_to_csv(commits, edges)
+        with open("repo.txt", "w") as f:
+            f.write(sys.argv[1])
 
